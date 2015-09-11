2133fc6bcaf2fbcea18aa38e67620496258fc81d
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-09-11 10:09:27 -0700

    add jQuery for display page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+//JavaScript:
 var findReplace = function(string, subString, replaceWith) {
     var result = string.replace(subString, replaceWith);
     return result;
-}
+};
+
+//jQuery for page display:
+$(document).ready(function() {
+    $("form#find-replace").submit(function(event) {
+
+        //Get variables from form:
+        var string = $("input#string").val();
+        var subString = $("input#subString").val();
+        var replaceWith = $("input#replaceWith").val();
+
+        //Variables to be displayed:
+        var result = findReplace(string, subString, replaceWith);
+        $(".result-replaced").text(result);
+        $(".original-string").text(string);
+
+        //Show #result div on form submit:
+        $("#result").show();
+        event.preventDefault();
+    });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
-describe ('findReplace', function() {
-    it('uses .replace to find and replace a word in a string', function() {
-        expect(findReplace('Hello world!', 'world', 'universe')).to.equal('Hello universe!');
+describe ("findReplace", function() {
+    it("uses .replace to find and replace a word in a string", function() {
+        expect(findReplace("Hello world!", "world", "universe")).to.equal("Hello universe!");
     });
 });
 
-describe ('findReplace', function() {
-    it('finds a multi-word substring and replaces it with another', function() {
-        expect(findReplace('Hey there, friend!', 'Hey there', 'See you soon')).to.equal('See you soon, friend!');
+describe ("findReplace", function() {
+    it("finds a multi-word substring and replaces it with another", function() {
+        expect(findReplace("Hey there, friend!", "Hey there", "See you soon")).to.equal("See you soon, friend!");
     });
 });
