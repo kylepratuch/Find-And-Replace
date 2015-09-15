c17f8ea9563bbbd8c1b991e3e0a4ee0d9cf0e583
Diane Douglas <diane.douglas1@gmail.com>
2015-09-15 09:51:59 -0700

    Add extra specs.

--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,25 @@
 describe ("findReplace", function() {
+
+    it("replaces a one letter word with another one letter word.", function() {
+        expect(findReplace("a", "a", "b")).to.equal("b");
+    });
+
+    it("replaces a word with another word.", function() {
+        expect(findReplace("hello", "hello", "hi")).to.equal("hi");
+    });
+
     it("uses .replace to find and replace a word in a string", function() {
         expect(findReplace("Hello world!", "world", "universe")).to.equal("Hello universe!");
     });
-});
 
-describe ("findReplace", function() {
     it("finds a multi-word substring and replaces it with another", function() {
         expect(findReplace("Hey there, friend!", "Hey there", "See you soon")).to.equal("See you soon, friend!");
     });
+
+    //multiple matches
+    //punctuation
+    //capitalization
+    //negative cases: "hi there my friend", "banana" -> ?
+    //partial matches: "hello", "he", should not produce a match.
+
 });
